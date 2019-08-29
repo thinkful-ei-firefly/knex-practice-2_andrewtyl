cf3d731b30d057739574761b2118d8abe8ff0b45
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-08-29 13:00:20 -0500

    blogful sql files

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+insert into blogful_articles (title, date_published, content)
+values
+    ('Top 10 Numbers from One through Ten', TO_TIMESTAMP('201705251200', 'YYYYMMDDHH24MI'), 'https://youtu.be/giS0BrfXUmM'),
+    ('15 Hedgehogs With Things That Look Like Hedgehogs', TO_TIMESTAMP('201302182109', 'YYYYMMDDHH24MI'), 'I think I need to get my eyes checked or something. Im having some difficulty telling these things apart.'),
+    ('Which Disney Non-Princess Are You?', TO_TIMESTAMP('201606191324', 'YYYYMMDDHH24MI'), 'Because lets be honest, royalty is overrated.'),
+    ('20 Slightly Incorrect Names For Food', TO_TIMESTAMP('201501231547', 'YYYYMMDDHH24MI'), 'Love a good milk snake.'),
+    ('Which Ousted Arab Spring Ruler Are You?', TO_TIMESTAMP('201403061621', 'YYYYMMDDHH24MI'), 'The "Arab Spring" unseated unpopular rulers across the Middle East and North Africa. Which one are you really?'),
+    ('22 Celebrities That Look Nothing Alike', TO_TIMESTAMP('201405011545', 'YYYYMMDDHH24MI'), 'Here are 11 sets of famous people that really just look so different.'),
+    ('10 Signs That Your Cat Is A Wizard', TO_TIMESTAMP('201303181712', 'YYYYMMDDHH24MI'), 'An easy guide to identifying a feline Dumbledore.'),
+    ('19 Things Worse Than Finding Out That Beyoncé Lip-Synched The National Anthem', TO_TIMESTAMP('201301221152', 'YYYYMMDDHH24MI'), 'AHHHH WHY WHY WHY!!!'),
+    ('20 Things That Will Make You Say NOOOOOOO!', TO_TIMESTAMP('201302081421', 'YYYYMMDDHH24MI'), 'Warning: You might have a heart attack while looking at this.'),
+    ('What Is Your Inner Potato?', TO_TIMESTAMP('201402191442', 'YYYYMMDDHH24MI'), 'Hey there, spudmuffin.'),
+    ('The Top 10 DUMBEST BuzzFeed Lists Youre EMBARRASSED To Say You CLICKED', TO_TIMESTAMP('201307091414', 'YYYYMMDDHH24MI'), 'LOL FAIL!'),
+    ('26 Face Swaps That Will Make You Ridiculously Uncomfortable', TO_TIMESTAMP('201410241527', 'YYYYMMDDHH24MI'), 'MAKE IT STOP.'),
+    ('Can We Guess Your Age With Ridiculous Questions?', TO_TIMESTAMP('201602161101', 'YYYYMMDDHH24MI'), 'Oh, most definitely.'),
+    ('16 Sassy Tweets From The Nations 16th Largest School District', TO_TIMESTAMP('201304291838', 'YYYYMMDDHH24MI'), 'The Wake County Public School System in North Carolina is so done with these kids.'),
+    ('What Type Of Fall Sweater Are You?', TO_TIMESTAMP('201509250942', 'YYYYMMDDHH24MI'), 'The reason for the season is finally here.'),
+    ('13 Potatoes That Look Like Channing Tatum', TO_TIMESTAMP('201310241520', 'YYYYMMDDHH24MI'), 'More like Channing Tater, amirite?'),
+    ('Which Possible Illuminati Member Are You?', TO_TIMESTAMP('201403170650', 'YYYYMMDDHH24MI'), 'This is the new world order.'),
+    ('Top 10 Lists', TO_TIMESTAMP('201905050505', 'YYYYMMDDHH24MI'), 'Number 1, this one.'),
+    ('Top 10 days of the week', TO_TIMESTAMP('201908291056', 'YYYYMMDDHH24MI'), 'https://www.youtube.com/watch?v=5v-ugxjh53Q'),
+    ('why i used almosts all awful buzzfeed articles to populate this table', now(), 'idk, lol')
+;
